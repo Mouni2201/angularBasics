3e5a22381ff5289cce4a76e6845b4e39b995b351
Chanda Mounica <pvryar.4@gmail.com>
2025-08-08 19:04:54 +0530

    Angular basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-comp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularBasics';
+  title = 'AngularBasics';
+  projectName = "Angular";
+
+  concept1 = "String Interpolation and the syntax is {{}}";
+  concept2 = "Property Binding and syntax is []";
+
+  customerRole = "Principal";
+  InputType = "Checkbox";
+  isDisabled = true;
+
+  concept3 = "event binding is action and the syntax is ()";
+
+  btnClick(){
+     console.log("Button Clicked");
+   }
+
+   btnMouseOver(){
+      console.log("You are hovering over the button");
+   }
 }
---
+++ src/app/test-comp/test-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+// component is Namesapce
+//YOu have to inform to angular say that 
+//Hey pleae take it TestCompComponent as component
+//Then take component features here....
+//Basics ehtis
+//Comp - > html + ts + css along with name
+@Component({
+   selector    : 'app-test-comp',
+   templateUrl : './test-comp.component.html',
+   styleUrls   : ['./test-comp.component.css']
+})
+export class TestCompComponent {
+
+}
